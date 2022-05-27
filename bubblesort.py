61f9f197d41ac24f664b5ba37a8aae6627846fd5
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-05-27 21:47:02 +0300

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -17,9 +17,8 @@ def countSwaps(a):
     # Write your code here
     for i,j in product(range(len(a)),range(len(a)-1)):
              if a[j] > a[j + 1]:
-                temp = a[j]
-                a[j] = a[j + 1]
-                a[j + 1] = temp
+                a[j], a[j + 1] = a[j + 1], a[j]
+                
                 count+=1
     print("Array is sorted in %s swaps." %count)
     print("First Element:", a[0])
